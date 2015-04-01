66e730473a91065ebc1c454ef6aceb34de3df103
Joshua Boone <JBoonie231@yahoo.com>
2015-04-01 02:36:29 -0700

    Added various major game mechanics.
    Added spline scripts to handle paths and replace waypoints.  Added mouse
    click raycast to shoot targets.  Added camera focusing, and manual
    camera rotating.

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour 
+{
+	public bool focus;
+	public GameObject target;
+
+	// Use this for initialization
+	void Start () 
+	{
+		focus = false;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		Focus ();
+	}
+
+	public void Focus()
+	{
+		if (focus)
+			transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(target.transform.position - transform.position), .01f);
+		else
+			transform.localRotation = Quaternion.Slerp(transform.localRotation, Quaternion.identity, .01f);
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour 
+{
+	bool primaryFire;
+	float lookVertical;
+	float lookHorizontal;
+
+	RaycastHit hit;
+	float range = 50f;
+
+	// Use this for initialization
+	void Start () 
+	{
+		primaryFire = false;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		lookVertical = -Input.GetAxis("Vertical");
+		lookHorizontal = Input.GetAxis("Horizontal");
+		primaryFire = Input.GetButtonDown("Fire1");
+
+		TurnHead();
+
+		if (primaryFire)
+		{
+			PrimaryFire();
+		}
+	}
+
+	void PrimaryFire ()
+	{
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		if (Physics.Raycast(ray, out hit, range))
+		{
+			if (hit.collider.gameObject.tag == "Enemy")
+			{
+				Debug.Log("Boom. Headshot.");
+			}
+		}
+	}
+
+	void TurnHead ()
+	{
+		transform.localRotation = Quaternion.Slerp(transform.localRotation, Quaternion.Euler(90f*lookVertical, 90f*lookHorizontal, 0f), 5f);
+	}
+}
