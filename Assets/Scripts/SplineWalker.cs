7d19073f0758930750aa327c3cf0f36419178708
Bryce Kinney <brycekinney91@gmail.com>
2015-04-04 19:06:54 -0700

    Level 1 Paths
    I added level 1 paths(not all since engine room and hangar need to be
    discussed). I also put sphere colliders to pause the movement at
    designated portions. Check out the "pause" checkbox in the camera
    inspector to make the camera move after pausing at a sphere.

--- Assets/Scripts/SplineWalker.cs
+++ Assets/Scripts/SplineWalker.cs
@@ -8,39 +8,52 @@ public class SplineWalker : MonoBehaviour {
 
 	public bool lookForward;
 
+	public bool pause = false;
+
 	public SplineWalkerMode mode;
 
 	private float progress;
 	private bool goingForward = true;
 
 	private void Update () {
-		if (goingForward) {
-			progress += Time.deltaTime / duration;
-			if (progress > 1f) {
-				if (mode == SplineWalkerMode.Once) {
-					progress = 1f;
+		if (!pause) {
+			if (goingForward) {
+				progress += Time.deltaTime / duration;
+				if (progress > 1f) {
+					if (mode == SplineWalkerMode.Once) {
+						progress = 1f;
+					} else if (mode == SplineWalkerMode.Loop) {
+						progress -= 1f;
+					} else {
+						progress = 2f - progress;
+						goingForward = false;
+					}
 				}
-				else if (mode == SplineWalkerMode.Loop) {
-					progress -= 1f;
-				}
-				else {
-					progress = 2f - progress;
-					goingForward = false;
+			} else {
+				progress -= Time.deltaTime / duration;
+				if (progress < 0f) {
+					progress = -progress;
+					goingForward = true;
 				}
 			}
-		}
-		else {
-			progress -= Time.deltaTime / duration;
-			if (progress < 0f) {
-				progress = -progress;
-				goingForward = true;
-			}
-		}
 
-		Vector3 position = spline.GetPoint(progress);
-		transform.localPosition = position;
-		if (lookForward) {
-			transform.LookAt(position + spline.GetDirection(progress));
+			Vector3 position = spline.GetPoint (progress);
+			transform.localPosition = position;
+			if (lookForward) {
+				transform.LookAt (position + spline.GetDirection (progress));
+			}
 		}
 	}
+	void OnTriggerEnter(Collider other){
+		if (other.gameObject.tag == "stopPoint") {
+			pause = true;
+			Destroy(other.gameObject);
+			Debug.Log("STOP POINT DETECTED");
+
+		} 
+	}
+		
+		
+
+
 }
