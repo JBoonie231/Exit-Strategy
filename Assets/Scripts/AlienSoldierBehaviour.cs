d532ad87df29647c1eb3116c2b6d8c70e6b90597
Joshua Boone <JBoonie231@yahoo.com>
2015-04-15 20:25:47 -0700

    Created Alien Soldier Prefab
    Added Alien Soldier prefab and script.
    Added navmesh components.

---
+++ Assets/Scripts/AlienSoldierBehaviour.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AlienSoldierBehaviour : MonoBehaviour 
+{
+	public Transform target;
+	NavMeshAgent agent;
+
+	public GameObject weapon;
+	float health;
+
+	enum TopState {Idle, Engage, Flee, Die};
+	enum IdleState {Standing, Walking, Talking};
+	enum EngageState {Waiting, Moving, Shooting, Cover};
+
+
+	TopState topState;
+	IdleState idleState;
+	EngageState engageState;
+
+	// Use this for initialization
+	void Start () 
+	{
+		agent = GetComponentInParent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		agent.SetDestination(target.position);
+		switch (topState)
+		{
+			case TopState.Idle:
+				switch (idleState)
+				{
+
+				}
+				break;
+
+			case TopState.Engage:
+				switch (engageState)
+				{
+
+				}
+				break;
+
+			case TopState.Flee:
+				break;
+
+			case TopState.Die:
+				break;
+
+			default:
+				break;
+		}
+	}
+}
---
+++ Assets/Scripts/EnemyWaypointBehaviour.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyWaypointBehaviour : MonoBehaviour 
+{
+	public bool cover;
+	public bool hostile;
+	public bool idle;
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+}
