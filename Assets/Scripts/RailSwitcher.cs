991d609ec25e39640bbefe8a0763bfe215103ab9
Joshua Boone <JBoonie231@yahoo.com>
2015-04-18 15:31:53 -0700

    Adding Austin's and Bryce's scripts
    m9_fire and RailSwitcher

---
+++ Assets/Scripts/RailSwitcher.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RailSwitcher : MonoBehaviour {
+	public MotionPath motionPath;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/m9_Fire.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class m9_Fire : MonoBehaviour 
+{
+
+	public GameObject Bullet;
+	public AudioClip M9Fire;
+	public float Volume = 1.0f;
+
+	private AudioSource source;
+
+	void Awake ()
+	{
+		source = GetComponent<AudioSource> ();
+	}
+
+	void Update ()
+	{
+		if (Input.GetButtonDown ("Fire1")) 
+		{
+			GameObject clone;
+			clone = Instantiate (Bullet, transform.position, transform.rotation) as GameObject;
+
+			source.PlayOneShot(M9Fire, Volume);
+
+		}
+
+	}
+}
